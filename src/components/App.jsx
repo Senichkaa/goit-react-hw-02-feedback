2e4b4695c725a61965afe57c2262bcf621b38c7a
Senichkaa <super-kapanaiko406@ukr.net>
2023-08-23 19:02:06 +0300

    initial commit v2.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return (
+      <div>
+        <h2>Please leave your feedback</h2>
+        <button type="button">good</button>
+        <button type="button">neutral</button>
+        <button type="button">bad</button>
+        <h2>Statistics</h2>
+        <p>Good: </p>
+        <p>Neutral: </p>
+        <p>Bad: </p>
+        <p>Total Feedbacks: </p>
+        <p>Positive percentage: </p>
+      </div>
+    );
+  }
+}
